976035a89b5b6a413fc0bb59e3e8a19b0adbff48
prisha685 <77448105+prisha685@users.noreply.github.com>
2021-06-02 21:08:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+prediction_1="";
+Webcam.set({
+    width:350,
+    height:350,
+    image_format:'png',
+    png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+function capture_image(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2INdBoZXd/',modelLoaded);
+function modelLoaded() {
+    console.log('modelLoaded');
+}
